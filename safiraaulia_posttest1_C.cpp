ffe4753485b582280e1bfe508a77b0c13d59a91a
safiiraaulia <112611988+safiiraaulia@users.noreply.github.com>
2022-09-25 09:04:11 +0700

    Add files via upload

---
+++ safiraaulia_posttest1_C.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+  int myNum = 12;
+  cout << myNum << "\n";
+  float myFloat = 24.5;
+  cout << myFloat << "\n";
+  string greeting = "Hello world!";
+  cout << greeting << "\n";
+  char myGrade = 'R';
+  cout << myGrade << "\n";
+
+  return 0;
+}
